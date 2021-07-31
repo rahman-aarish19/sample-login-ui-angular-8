a96f5297b9fa9dcff5e06ed414b81966a8d925e7
Aarish Rahman <aarishrahman.1062@gmail.com>
2021-07-31 21:25:06 +0530

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,81 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AlertService, AuthenticationService } from '@/_services';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    templateUrl: 'login.component.html',
+})
+
+export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+    ipAddress: string = '';
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService,
+        private _http: HttpClient
+    ) {
+        // redirect to home if already logged in
+        if (this.authenticationService.currentUserValue) {
+            this.router.navigate(['/']);
+        }
+    }
+
+    ngOnInit() {
+        this.getClientIPAddress();
+
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    // to get client's ip address.
+    getClientIPAddress() {
+        this._http.get('https://geolocation-db.com/json/').subscribe((res: any) => {
+            this.ipAddress = res.IPv4; console.log(res); console.log('21');
+        }, err => {
+            console.log(err);
+        })
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.loginForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.authenticationService.login(this.f.username.value, this.f.password.value, this.ipAddress)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+}
